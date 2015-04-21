8c569d1e234382ef833cd79a0acc62ba194429b0
Jesus <ctw@ctwhome.com>
2015-04-21 21:15:56 +0200

    animate

--- App/scripts/controllers/login-page-controller.js
+++ App/scripts/controllers/login-page-controller.js
@@ -8,56 +8,50 @@
  * Controller of the stocksApp
  */
 
-App.controller('login-page-controller',['$scope', 'Auth', function($scope, Auth)
-{
+App.controller('login-page-controller', ['$scope', 'Auth', function ($scope, Auth) {
+    // Fixme these variables
     $scope.auth = Auth;
-
     $scope.forgetForm = true;
-
     $scope.login = false;
 
-    $scope.forgetPassword = function(){
-        console.log("hola")
+    // Put the focus in the username
+    document.getElementById("username").focus();
+
+
+    /**
+     * Forgoten password
+     */
+    $scope.stateLogin = true;
+    $scope.animateBox = "";
+    $scope.toggleLogin = function () {
+        $scope.stateLogin = $scope.stateLogin === false ? true : false;
+        if ($scope.stateLogin){
+            document.getElementById("username").focus();
+            $scope.animateBox = "";
+        }
+        else{
+            document.getElementById("loginmail").focus();
+            $scope.animateBox = "tada";
+        }
+
     }
-    //
-    //// hide error messages until 'submit' event
-    //$scope.submitted = false;
-    //// hide success message
-    //$scope.showMessage = false;
-    //// method called from shakeThat directive
-    //$scope.submit = function() {
-    //    // show success message
-    //    $scope.showMessage = true;
-    //};
-
-
-    $scope.flip ="";
-        $scope.changeFlip = function(){
-            if ($scope.flip == "")
-                $scope.flip = "flip";
-            else
-                $scope.flip = "";
-        };
-
-
-      $scope.alerts = [
-            //{ type: 'success', msg: 'Well done! You successfully read this important alert message.' }
-        ];
-
 
     /**
      * Add alert
      */
-        $scope.addAlert = function() {
-            $scope.alerts.push({msg: 'Another alert!'});
-        };
+    $scope.alerts = [
+        //{ type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+    ];
+    $scope.addAlert = function () {
+        $scope.alerts.push({msg: 'Another alert!'});
+    };
     /**
      * Close alert
      * @param index
      */
-        $scope.closeAlert = function(index) {
-            $scope.alerts.splice(index, 1);
-        };
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+    };
 
 
 }])
